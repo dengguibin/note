87d368892cc07bccd5c807703b2145b7f7f9256a
dgb <dgb@foxmail.com>
2019-03-10 20:35:16 +0800

    ee

--- src/multithread/com/Test.java
+++ src/multithread/com/Test.java
@@ -1,14 +1,24 @@
 package multithread.com;
 
 import java.util.Calendar;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
     public static void main(String[] args) {
-        Calendar cal1 = Calendar.getInstance();
 
-        cal1.add(Calendar.SECOND, 60);
-
-        int second3later = (int) (cal1.getTimeInMillis() / 1000);
-        System.out.println(second3later);
+//        Calendar cal1 = Calendar.getInstance();
+//
+//        cal1.add(Calendar.SECOND, 60);
+//
+//        int second3later = (int) (cal1.getTimeInMillis() / 1000);
+//        System.out.println(second3later);
+//        Executors.newFixedThreadPool(10).submit(new Callable<Object>() {
+//            @Override
+//            public Object call() throws Exception {
+//                return null;
+//            }
+//        }).get
     }
 }
